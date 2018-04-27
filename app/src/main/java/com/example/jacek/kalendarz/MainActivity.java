e9c1b32e0e063301fd4580a8981a2a6cbcf29105
jacek3107 <jacek3107.ja@gmail.com>
2018-04-27 17:14:18 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/jacek/kalendarz/MainActivity.java
+++ app/src/main/java/com/example/jacek/kalendarz/MainActivity.java
@@ -171,4 +171,17 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     //*******************************************************************************************
+    
+    /*
+    private void chooseDay(){
+	CalendarView calendarView = findViewById(R.id.my_calendar)
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                data = dayOfMonth + "/" + month + "/" + year;
+            }
+        });
+    }
+    */
+    
 }
